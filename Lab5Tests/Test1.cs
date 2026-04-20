a82ccdcd3bf562d8bec1228e4b60bdebb7a66ea9
Zach Steward <stewardz@etsu.edu>
2026-04-19 22:21:43 -0400

    force fail

--- Lab5Tests/Test1.cs
+++ Lab5Tests/Test1.cs
@@ -147,7 +147,7 @@ namespace Lab5.Tests
 
             var result = service.ReturnBook(user.Id, book.Id);
 
-            Assert.IsFalse(false);
+            Assert.IsTrue(false);
         }
     }
 }
